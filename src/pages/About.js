f342e5879c7406c4e638ebc21818a092bdc8fd7a
William Buechele <buechelewd@gmail.com>
2021-05-24 09:35:54 -0600

    Completed Cocktail Database project. Uses React routing to create a website with a searchable list of cocktails

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum ex quam
+        molestiae quisquam ut sequi, harum nulla, magni quia, illo nesciunt
+        accusamus earum incidunt ipsa amet temporibus animi dolores! Impedit
+        porro hic consequatur, minus beatae ea perferendis odio eveniet maxime.
+      </p>
+    </section>
+  );
+};
+
+export default About;
